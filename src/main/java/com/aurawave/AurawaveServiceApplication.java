204519dd1d618798fcd20b6a02c9a01a24b8cfc7
Pedro Certo <113721862+pedroamcerto@users.noreply.github.com>
2025-06-10 00:09:18 -0300

    first commit

---
+++ src/main/java/com/aurawave/AurawaveServiceApplication.java
@@ -0,0 +1,11 @@
+package com.aurawave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AurawaveServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AurawaveServiceApplication.class, args);
+	}
+}
